2d9815c7d09cfe5142c4eac9105cee5ac17415d6
joaofp <jvfpdaemoon1@gmail.com>
2022-09-13 16:14:54 -0300

    template: done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserContext from "./Context/UserContext";
+import Cart from "./Pages/Cart";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/Signup";
+import WelcomePage from "./Pages/WelcomePage";
+import { getUserData } from "./Services/UserData";
+
+
 function App() {
+  const [login, setLogin] = useState(getUserData());
 
   return (
-    <div></div>
+    <BrowserRouter>
+      <UserContext.Provider value={{ login, setLogin }}>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/home' element={<WelcomePage />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </UserContext.Provider>
+    </BrowserRouter>
   )
 }
 
