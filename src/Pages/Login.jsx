732f4699ea7580f8e16e7e5cf7b212e8dfd617f0
GiovanneAndrade <vanne.as@hotmail.com>
2022-09-15 12:11:57 -0300

    Finalizando Resumo do carrinho no Front

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,15 @@
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+
+
 export default function Login(){
     return (
-        <><div>TESTANDO</div></>
+        <><div>TESTANDO
+            <Link to={"/cart"}>
+              <button>cart</button>
+            </Link>
+            
+        </div></>
         
     )
 }
