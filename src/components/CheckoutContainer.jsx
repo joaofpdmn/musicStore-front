36456a8013460cc7a1f5c2861afeaa10e7694290
joaofp <jvfpdaemoon1@gmail.com>
2022-09-16 17:44:47 -0300

    user context not working: fixed;starting checkout page

--- src/components/CheckoutContainer.jsx
+++ src/components/CheckoutContainer.jsx
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 const CheckoutContainer = styled.div` 
 display: flex;
+background-color: #ffffff;
+padding: 16px;
+box-sizing: border-box;
+width: 100%;
+height: 100%;
 
 h1{
     color: #b8b6b6;
