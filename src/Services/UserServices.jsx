6fe98a044adf8ff7e347bc9169533feedd3bc4fb
GiovanneAndrade <vanne.as@hotmail.com>
2022-09-18 10:00:57 -0300

    juntando rotas

--- src/Services/UserServices.jsx
+++ src/Services/UserServices.jsx
@@ -12,12 +12,12 @@ function signUpRequest(body){
 
 
 function getProducts(){
-    const promise=axios.get(`${BaseURL}/list`);
+    const promise=axios.get(`${APIprefix}/list`);
     return promise;
 }
 
 function getSearch(e){
-    const promise=axios.get(`${BaseURL}/list/${e}`);
+    const promise=axios.get(`${APIprefix}/list/${e}`);
     return promise;
 }
 function checkoutRequest(body){
