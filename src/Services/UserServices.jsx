27b14eee42590f380566b38f9536bf02fe31ba16
YessicaStern <yessica_stern@hotmail.com>
2022-09-18 09:43:56 -0300

    consertando errinho

--- src/Services/UserServices.jsx
+++ src/Services/UserServices.jsx
@@ -5,24 +5,19 @@ const APIprefix = 'https://music-store-driven.herokuapp.com';
 function loginRequest(body){
     return axios.post(`${APIprefix}/login`, body);
 }
-
 function signUpRequest(body){
     return axios.post(`${APIprefix}/signup`, body);
 }
-
-
 function getProducts(){
     const promise=axios.get(`${BaseURL}/list`);
     return promise;
 }
-
 function getSearch(e){
     const promise=axios.get(`${BaseURL}/list/${e}`);
     return promise;
-
+}
 function checkoutRequest(body){
     return axios.post(`${APIprefix}/checkout`, body);
-
 }
 
 export { 
@@ -30,4 +25,5 @@ export {
     signUpRequest,
     getProducts,
     getSearch,
+    checkoutRequest
 }
